2153f12e790870a4e8e3c679d353ea7539c01526
Nnamdi-Echegz <121641724+Nnamdi-Echegz@users.noreply.github.com>
2023-07-04 14:15:29 -0400

    navBar open and collapse fix

---
+++ navigationBar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidemnu").style.width = "240px";
+    document.getElementById("pagebdy").style.marginLeft = "240px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidemnu").style.width = "0";
+    document.getElementById("pagebdy").style.marginLeft= "0";
+  }
